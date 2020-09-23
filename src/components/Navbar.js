447e6d0ee5cccf91904dd598a24d0872dbc7b336
MoreCoke <seed45699@gmail.com>
2020-09-23 15:49:23 +0800

    Fix navbar closing failed issue and styles
    修復點選導覽列內容，導覽列無法關閉的問題，刪除 customnav-pt 將其樣式移到root 上

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import $ from "jquery";
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -15,12 +16,21 @@ export default class Navbar extends Component {
     }));
   };
 
+  closeNav = () => {
+    $("#mynav").collapse("hide");
+    this.setState({ isNavOpen: false });
+  };
+
   render() {
     const { isNavOpen } = this.state;
     return (
       <nav className="customnav navbar-expand-lg">
-        <Link className="logo" to="/guest/home">
-          {/* <img src="`${publicPath}img/logo.png`" width="100px" alt /> */}
+        <Link className="logo text-decoration-none" to="/guest/home">
+          <img
+            src={`${process.env.PUBLIC_URL}/img/logo.png`}
+            width="100px"
+            alt="logo"
+          />
           速達通訊
         </Link>
         <button
@@ -40,13 +50,19 @@ export default class Navbar extends Component {
         <div className="collapse navbar-collapse" id="mynav">
           <ul className="customnav-group">
             <li>
-              <Link to="/guest/promotion">優惠活動</Link>
+              <Link to="/guest/promotion" onClick={this.closeNav}>
+                優惠活動
+              </Link>
             </li>
             <li>
-              <Link to="/guest/aboutus">關於我們</Link>
+              <Link to="/guest/aboutus" onClick={this.closeNav}>
+                關於我們
+              </Link>
             </li>
             <li>
-              <Link to="/guest/productlist/:brand">商品列表</Link>
+              <Link to="/guest/productlist/:brand" onClick={this.closeNav}>
+                商品列表
+              </Link>
             </li>
           </ul>
         </div>
